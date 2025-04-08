b061faeab4f0582a9177a9ac290d8b9cb7af1ea3
Yasrab30 <yasrabsehar@gmail.com>
2025-04-08 11:58:15 +0530

    "Updated DBConnection.java file"

--- Sbpdcl/src/main/java/com/SBPDCL/util/DBConnection.java
+++ Sbpdcl/src/main/java/com/SBPDCL/util/DBConnection.java
@@ -1,24 +1,32 @@
 package com.SBPDCL.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
-	static  String url = "jdbc:mysql://localhost:3306/projectdb";
-    static String user = "root";
-    static String password = "da_1014";
-    
+	
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException
     {
-    	 try {
-    		    Class.forName("com.mysql.cj.jdbc.Driver");
-    		   } catch (ClassNotFoundException e) {
-    		    // TODO Auto-generated catch block
-    		    e.printStackTrace();
-    		   }
-    		      Connection connection =DriverManager.getConnection(url,user,password);
-    		      return connection;
-    		     }
+    	Properties props=new Properties();
+    	try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                System.out.println("Unable to find db.properties");
+                return null;
+            }
+            props.load(input);
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(url, user, password);
+    }
 }
