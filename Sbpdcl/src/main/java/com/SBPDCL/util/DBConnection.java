c7a71e31c0f8e730911889bec6af1eecb1be5296
Yasrab30 <yasrabsehar@gmail.com>
2025-04-08 21:08:16 +0530

    "Update DBConnection"

--- Sbpdcl/src/main/java/com/SBPDCL/util/DBConnection.java
+++ Sbpdcl/src/main/java/com/SBPDCL/util/DBConnection.java
@@ -1,7 +1,7 @@
 package com.SBPDCL.util;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,22 +11,35 @@ public class DBConnection {
 	
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException
+    	
     {
-    	Properties props=new Properties();
-    	try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("db.properties")) {
-            if (input == null) {
-                System.out.println("Unable to find db.properties");
+    	Connection conn = null;
+
+        try {
+            Properties props = new Properties();
+            InputStream in = DBConnection.class.getClassLoader().getResourceAsStream("db.properties");
+            URL url1 = DBConnection.class.getClassLoader().getResource("db.properties");
+            System.out.println("Found at: " + url1);
+            if (in == null) {
+                System.out.println("db.properties not found in classpath!");
                 return null;
             }
-            props.load(input);
-        } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	String url = props.getProperty("db.url");
-        String user = props.getProperty("db.user");
-        String password = props.getProperty("db.password");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(url, user, password);
+
+            props.load(in);
+
+            String driver = props.getProperty("db.driver");
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
     }
+
 }
